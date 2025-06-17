e039897618f62d7f9c58c134ac2ec9b69844a7ff
Carlos García <carlos.garcia@silacins.com>
2025-06-17 09:06:45 -0600

    update to .net 9

---
+++ tests/ApiUnitTests/BankAccountDtoTests.cs
@@ -0,0 +1,14 @@
+using Api.Dtos;
+
+namespace ApiUnitTests;
+
+[TestClass]
+public class DtoTests {
+
+    [TestMethod]
+    public void TestConvertion() {
+        Assert.IsTrue(true);
+        
+    }
+
+}
