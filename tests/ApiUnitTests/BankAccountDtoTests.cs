a966d0dd7b5e01edffe03d0fc5261108d49c0b90
Carlos García <carlos.garcia@silacins.com>
2025-06-17 09:40:55 -0600

    add unit test to demonstrate usage of Expression<Func<T,R>>

--- tests/ApiUnitTests/BankAccountDtoTests.cs
+++ tests/ApiUnitTests/BankAccountDtoTests.cs
@@ -1,4 +1,5 @@
 using Api.Dtos;
+using Domain.Models;
 
 namespace ApiUnitTests;
 
@@ -7,8 +8,25 @@ public class DtoTests {
 
     [TestMethod]
     public void TestConvertion() {
-        Assert.IsTrue(true);
-        
+
+        // Arrange
+        var bankAccount = new BankAccount
+        {
+            Id = 1,
+            AccountNumber = "123456789",
+            AccountBalance = 1000.00m,
+            AccountHolder = "John Doe",
+            DepositHistory = string.Empty,
+            WithdrawalHistory = string.Empty,
+        };
+
+        // Act
+        var result = BankAccountDto.FromBankAccount.Compile().Invoke(bankAccount);
+
+        Assert.IsTrue(result is BankAccountDto);
+        Assert.AreEqual(bankAccount.Id, result.Id);
+        Assert.AreEqual(bankAccount.AccountNumber, result.AccountNumber);
+        Assert.AreEqual(bankAccount.AccountBalance, result.AccountBalance);
     }
 
 }
